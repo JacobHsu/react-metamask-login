1139e30ab5aa3feb5597f0b595ef4329a114b67f
jacobhsu <jacob.hsu.tw@gmail.com>
2022-08-09 17:49:05 +0800

    fix: config-overrides web3 webpack5

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Reference
+
+issues [Web3 Webpack<5](https://github.com/JacobHsu/react-notes/blob/main/web3.md) config-overrides
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Web3 from 'web3';
 import './App.css';
+const MetamaskLogo = 'https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg';
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    function checkConnectedWallet() {
+      const userData = JSON.parse(localStorage.getItem('userAccount'));
+      if (userData != null) {
+        setUserInfo(userData);
+        setIsConnected(true);
+      }
+    }
+    checkConnectedWallet();
+  }, []);
+
+  const detectCurrentProvider = () => {
+    let provider;
+    if (window.ethereum) {
+      provider = window.ethereum;
+    } else if (window.web3) {
+      // eslint-disable-next-line
+      provider = window.web3.currentProvider;
+    } else {
+      console.log(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
+    }
+    return provider;
+  };
+
+  const saveUserInfo = (ethBalance, account, chainId) => {
+    const userAccount = {
+      account: account,
+      balance: ethBalance,
+      connectionid: chainId,
+    };
+    window.localStorage.setItem('userAccount', JSON.stringify(userAccount)); //user persisted data
+    const userData = JSON.parse(localStorage.getItem('userAccount'));
+    setUserInfo(userData);
+    setIsConnected(true);
+  };
+
+  const onConnect = async () => {
+    try {
+      const currentProvider = detectCurrentProvider();
+      if (currentProvider) {
+        if (currentProvider !== window.ethereum) {
+          console.log(
+            'Non-Ethereum browser detected. You should consider trying MetaMask!'
+          );
+        }
+        await currentProvider.request({ method: 'eth_requestAccounts' });
+        const web3 = new Web3(currentProvider);
+        const userAccount = await web3.eth.getAccounts();
+        const chainId = await web3.eth.getChainId();
+        const account = userAccount[0];
+        let ethBalance = await web3.eth.getBalance(account); // Get wallet balance
+        ethBalance = web3.utils.fromWei(ethBalance, 'ether'); //Convert balance to wei
+        saveUserInfo(ethBalance, account, chainId);
+        if (userAccount.length === 0) {
+          console.log('Please connect to meta mask');
+        }
+      }
+    } catch (err) {
+      console.log(
+        'There was an error fetching your accounts. Make sure your Ethereum client is configured correctly.'
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>React dApp authentication with Web3.js and Metamask</h1>
       </header>
+      <div className="app-wrapper">
+        {!isConnected && (
+          <div>
+            <img src={MetamaskLogo} alt="meta mask logo" />
+            <button className="app-buttons__login" onClick={onConnect}>
+              Connect to MetaMask
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
